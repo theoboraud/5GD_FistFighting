7756b7f8ce089ef2d3fc77487b76496c3a152921
theoboraud <theoboraud@outlook.com>
2021-10-15 10:50:07 +0200

    Added rotate cooldown

--- Assets/Scripts/Gameplay/RotateBehaviour.cs
+++ Assets/Scripts/Gameplay/RotateBehaviour.cs
@@ -9,10 +9,15 @@ public class RotateBehaviour : MonoBehaviour
     private Rigidbody2D rb;
 
     private const float CONSTANT_rotateValue = 90f;      // How much the character turns when using rotation
-    private const float CONSTANT_rotateSpeed = 30f;                     //
+    private const float CONSTANT_rotateSpeed = 20f;
 
     private int rotateDir = 0;                          // Variable containing the trigonometric direction (1 for left, -1 for right)
     private float rotateValue = 0;                      // Variable containing the value of rotation to apply left
+
+    private bool isRotating = false;
+    private bool onCooldown = false;
+    private bool cooldownRoutineRunning = false;
+    private float rotateCooldown = 0.5f;
 
     private void Start()
     {
@@ -21,33 +26,62 @@ public class RotateBehaviour : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(rotateValue > 0)
+        if (rotateValue > 0)
         {
+            rotateValue = Mathf.Clamp(rotateValue, CONSTANT_rotateSpeed, CONSTANT_rotateValue);
             rb.rotation += rotateDir * CONSTANT_rotateSpeed;
             rotateValue -= CONSTANT_rotateSpeed;
         }
-        else
+
+        if (isRotating && rotateValue <= 0)
         {
+            isRotating = false;
             rotateDir = 0;
             rotateValue = 0;
+        }
+
+        if (onCooldown && !isRotating && !cooldownRoutineRunning)
+        {
+            StartCoroutine(RotateCooldown());
         }
     }
 
     public void RotateRight(InputAction.CallbackContext _value)
     {
-        if(rotateValue == 0)
+        if (CanRotate())
         {
             rotateDir = -1;
             rotateValue = CONSTANT_rotateValue;
+            isRotating = true;
+            onCooldown = true;
         }
     }
 
     public void RotateLeft(InputAction.CallbackContext _value)
     {
-        if(rotateValue == 0)
+        if (CanRotate())
         {
             rotateDir = 1;
             rotateValue = CONSTANT_rotateValue;
+            isRotating = true;
+            onCooldown = true;
         }
+    }
+
+    private IEnumerator RotateCooldown()
+    {
+        print("RotateCooldown started");
+        cooldownRoutineRunning = true;
+
+        yield return new WaitForSeconds(rotateCooldown);
+
+        cooldownRoutineRunning = false;
+        onCooldown = false;
+        print("RotateCooldown over");
+    }
+
+    private bool CanRotate()
+    {
+        return !onCooldown && !isRotating;
     }
 }
