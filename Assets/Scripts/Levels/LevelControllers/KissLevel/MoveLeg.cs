91248cd4249aa026cb81a305810eecb25a20abcb
chenshuo-code <77827637+chenshuo-code@users.noreply.github.com>
2021-11-27 11:53:28 +0100

    Move Leg update

--- Assets/Scripts/Levels/LevelControllers/KissLevel/MoveLeg.cs
+++ Assets/Scripts/Levels/LevelControllers/KissLevel/MoveLeg.cs
@@ -8,6 +8,8 @@ public class MoveLeg : MonoBehaviour
     public bool IsLeftLeg;
 
     private bool isActiveForce = false;
+    private bool isLegMove = false;
+
     private float rotationZ;
     private float coefRotate;
     private void Start()
@@ -15,28 +17,33 @@ public class MoveLeg : MonoBehaviour
         rotationZ = transform.rotation.eulerAngles.z;
         if (IsLeftLeg) coefRotate = 1; else coefRotate = -1;
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        print(rotationZ);
-        transform.DORotate(new Vector3 (0,0, rotationZ + 20),1f);
-
         if (isActiveForce)
         {
             collision.rigidbody.AddForce((collision.transform.position - transform.position) * 10, ForceMode2D.Impulse);
-
         }
-        //Invoke("LegKick", 1);
-
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!isLegMove)
+        {
+            transform.DORotate(new Vector3(0, 0, rotationZ + coefRotate * 20), 1f).OnComplete(LegKick);
+            isLegMove = true;
+        }
+        if (isActiveForce)
+        {
+            collision.rigidbody.AddForce((collision.transform.position - transform.position) * 10, ForceMode2D.Impulse);
+        }
     }
     private void LegKick()
     {
         isActiveForce = true;
-        transform.DORotate(new Vector3(0, 0, rotationZ - coefRotate * 20), 1f);
-        Invoke("ReturnLeg", 1);
+        transform.DORotate(new Vector3(0, 0, rotationZ - coefRotate * 20), 1f).OnComplete(ReturnLeg);
     }
     private void ReturnLeg()
     {
-        transform.DORotate(new Vector3(0, 0, 400), 1f);
+        transform.DORotate(new Vector3(0, 0, rotationZ), 1f).OnComplete(()=>isLegMove = false);
         isActiveForce = false;
     }
 }
