9d1be6638747d2d673eb780255e1d984a6f60289
chenshuo-code <lichenshuome@hotmail.com>
2025-04-01 16:52:40 +0200

    Clean code for stun timer calculation (No function change)

--- Assets/Scripts/Gameplay/Player/PlayerStunBehaviour.cs
+++ Assets/Scripts/Gameplay/Player/PlayerStunBehaviour.cs
@@ -32,11 +32,12 @@ public class PlayerStunBehaviour : MonoBehaviour
             {
                 StartStunState();
             }
-            _playerController.StunTimer += Time.deltaTime;
+            float stunRecoveryTime = GlobalSettings.StunRecoveryTime;
+            //_playerController.StunTimer += Time.deltaTime;
             Debug.Log(_playerController.StunTimer);
-            float addedTimeBasedOnStunAccumulation = StunAccumulation * (0.2f * _playerController.StunRecoveryTime);
+            float addedTimeBasedOnStunAccumulation = StunAccumulation * (0.2f * stunRecoveryTime);
             //Check if timer has gone above the required stun time
-            if (_playerController.StunTimer >= GlobalSettings.StunRecoveryTime + addedTimeBasedOnStunAccumulation)
+            if (_playerController.StunTimer >= stunRecoveryTime + addedTimeBasedOnStunAccumulation)
             {
                 StopStunState();
             }
